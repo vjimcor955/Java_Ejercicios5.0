425a702b7dc1daf8cea1b693dd71a03ccfbbf781
Judith <jcoeque299@g.educaand.es>
2023-10-02 17:02:14 +0200

    Todos los ejercicios impares hechos

--- src/ejercicio1.java
+++ src/ejercicio1.java
@@ -1,5 +1,16 @@
-//public class ejercicio1 {
-//    public static void main(String[] args) {
-//        int[] num = {39,-2,null,null,0,null,14,null,5,120,null,null};
-//    }
-//}
+import java.util.Arrays;
+
+public class ejercicio1 {
+    public static void main(String[] args) {
+        int[] numeros = new int[10];
+        numeros[0] = 39;
+        numeros[1] = -2;
+        numeros[4] = 0;
+        numeros[6] = 14;
+        numeros[8] = 5;
+        numeros[9] = 120;
+        System.out.println(Arrays.toString(numeros));
+
+        //Los valores no inicializados se reemplazan por ceros
+    }
+}
